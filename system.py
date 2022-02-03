388871e2a591f360197405ecf0a099353e171bd7
timothijoe <zt1301112@gmail.com>
2022-02-03 21:52:27 +0800

    ok

---
+++ system.py
@@ -0,0 +1,12 @@
+sudo useradd -m user-name 
+
+sudo passwd user-name
+
+
+安装anaconda
+
+wget -O - https://www.anaconda.com/distribution/ 2>/dev/null | sed -ne 's@.*\(https:\/\/repo\.anaconda\.com\/archive\/Anaconda3-.*-Linux-x86_64\.sh\)\">64-Bit (x86) Installer.*@\1@p' | xargs wget
+
+
+
+
