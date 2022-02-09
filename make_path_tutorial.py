a9d9acde6fc4c6bdc0c1e81004ba4ef68878d9ca
timothijoe <zt1301112@gmail.com>
2022-02-09 15:44:51 +0800

    ok

--- make_path_tutorial.py
+++ make_path_tutorial.py
@@ -38,3 +38,37 @@ new_folder_path = os.path.join(target_folder_dir, new_folder_name)
 print(new_folder_path)
 if not os.path.exists(new_folder_path):
     os.makedirs(new_folder_path)
+
+
+
+def mk_logdir(params):
+    path1 = 'result'
+    path2 = 'result/{}/ckpt'.format(params.exp_name)
+    path3 = 'result/{}/log'.format(params.exp_name)
+    path4 = 'result/{}/images'.format(params.exp_name)
+    if not os.path.exists(path1):
+        os.makedirs(path1)
+    if not os.path.exists(path2):
+        os.makedirs(path2)
+    if not os.path.exists(path3):
+        os.makedirs(path3)
+    if not os.path.exists(path4):
+        os.makedirs(path4)
+
+关于读取一个文件，后把这个文件的名字作为另一个文件名字或者名字的一部分
+
+os.path.split('PATH') ： 
+（1） PATH是指一个文件的全路径作为参数
+（2） 如果给出的是一个目录和文件名，则输出路径和文件名
+（3） 如果给出的是一个目录名，则输出路径和为空的文件名
+实际上，该函数的分割并不只能，仅仅以PATH中最后一个'/'作为分隔负号，分隔后，将索引为0的视为目录（路径——，将索引1视作文件名
+
+使用basename()函数：
+import ps.path
+filePATH = '/home/hello.txt'
+x = os.path.basename(filePATH)
+
+' x = hello.txt'
+去后缀：
+os.path.splittext(x)[0]
+ 则分离文件名字和扩展名；返回默认(fname, fextension)元组，若[1]则返回后缀
