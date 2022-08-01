15500cd8360e66077d822bde18ae6d5422c5f01a
timothijoe <zt1301112@gmail.com>
2022-08-01 21:47:22 +0800

    ok

--- august_update.py
+++ august_update.py
@@ -3,7 +3,7 @@
 
 ssh zhoutong@jump01.sh.sensetime.com
 
-集群管控入口手册　　　https://confluence.sensetime.com/pages/viewpage.action?pageId=321143255
-入职 warmup 　　https://confluence.sensetime.com/pages/viewpage.action?pageId=324986592
-phx-sh38入口：　https://phoenix.sensetime.com/p/c-fw7fc:p-dckzb/workloads
-phx-sh1024入口：　　https://phoenix.sensetime.com/p/c-pfjwt:p-t2t2t/workloads
+[集群管控入口手册](https://confluence.sensetime.com/pages/viewpage.action?pageId=321143255)
+[入职 warmup](https://confluence.sensetime.com/pages/viewpage.action?pageId=324986592)
+[phx-sh38入口](https://phoenix.sensetime.com/p/c-fw7fc:p-dckzb/workloads)
+[phx-sh1024入口](https://phoenix.sensetime.com/p/c-pfjwt:p-t2t2t/workloads)
