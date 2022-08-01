3fe5705840fe229a86c691ab046810cd8d8e6b1a
timothijoe <zt1301112@gmail.com>
2022-08-01 21:34:49 +0800

    ok

--- august_update.py
+++ august_update.py
@@ -1,3 +1,7 @@
-1024　 集群：　10.198.34.31
-38    集群:  10.5.38.31
+1024　 集群：　10.198.34.31        ftp ---  10.198.34.11
+38    集群:  10.5.38.31           ftp ---  10.5.39.62
 
+ssh zhoutong@jump01.sh.sensetime.com
+
+集群管控入口手册　https://confluence.sensetime.com/pages/viewpage.action?pageId=321143255
+入职 warmup https://confluence.sensetime.com/pages/viewpage.action?pageId=324986592
