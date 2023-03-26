b2181e26e07a9c033825825c88df39fb385b3720
q517470045 <qi_senmao@163.com>
2023-03-26 22:21:02 +0800

    ok

---
+++ march2023.py
@@ -0,0 +1,8 @@
+1. 多传感器融合 imu & odometry
+https://github.com/udacity/robot_pose_ekf
+
+2. dist nav
+Move Beyond Trajectories: Distribution Space Coupling for Crowd Navigation
+https://github.com/MurpheyLab/DistNav
+
+3. 
