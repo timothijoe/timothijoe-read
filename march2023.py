18cc74e294ca2cc84b59220a83be89f70503b61f
timothijoe <zt1301112@gmail.com>
2023-03-27 23:45:10 +0800

    ok

--- march2023.py
+++ march2023.py
@@ -1,8 +1,15 @@
 1. 多传感器融合 imu & odometry
+
+https://github.com/cra-ros-pkg/robot_localization
 https://github.com/udacity/robot_pose_ekf
 
 2. dist nav
 Move Beyond Trajectories: Distribution Space Coupling for Crowd Navigation
 https://github.com/MurpheyLab/DistNav
 
-3. 
+3. irl drive github
+https://github.com/MCZhi/Driving-IRL-NGSIM
+https://github.com/MCZhi/Expert-Prior-RL
+https://github.com/MCZhi/DIPP
+
+https://github.com/d1024choi/traj-pred-irl
