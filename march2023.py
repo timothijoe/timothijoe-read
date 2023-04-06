57e9b617479efee274760c45c2e0c06e1f1774c8
timothijoe <zt1301112@gmail.com>
2023-04-06 15:00:42 +0800

    ok

--- march2023.py
+++ march2023.py
@@ -7,9 +7,22 @@ https://github.com/udacity/robot_pose_ekf
 Move Beyond Trajectories: Distribution Space Coupling for Crowd Navigation
 https://github.com/MurpheyLab/DistNav
 
+安装distnav遇到的问题：
+pip install distnav
+ pip install jaxlib
+pip install --upgrade jax jaxlib
+pip install jupyter
+pip install george
+jupyter notebook
+
+pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+
+
 3. irl drive github
 https://github.com/MCZhi/Driving-IRL-NGSIM
 https://github.com/MCZhi/Expert-Prior-RL
 https://github.com/MCZhi/DIPP
 
 https://github.com/d1024choi/traj-pred-irl
+
+
