e42dd93a118fb72d06f552216282b6d6233103f8
timothijoe <zt1301112@gmail.com>
2023-04-19 19:54:27 +0800

    ok

---
+++ april.py
@@ -0,0 +1,14 @@
+DI-engine:
+
+branch_2048_diengine:负责开发2048环境的,主要负责调试dqn,由于DI-engine自带的encoder不支持4x4x16,因此设定于此
+main: 目前lightzero用于此分支开发,由于并没有发现冲突,因此可以继续使用这个分支开发metadrive
+metadirve_branch, metadrive_feb_branch: 用于开发metadrive的模块,但似乎main也可以直接使用
+jinbo_nov21_branch: 用于ral当时的di-engine,可以使用它作为ral的复现
+
+
+
+metadrive:
+di_engine_branch : 用于开发后续的情况. 注意延续的是2023.2.10的官方Main分支
+
+
+ 
